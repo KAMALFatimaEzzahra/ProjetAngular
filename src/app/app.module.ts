b3abb6672b8355e965ccd055d06d70ff9e18128f
FatimaEzzahra KAMAL <fatimaezzahrakamal1996@gmail.com>
2020-02-22 18:20:51 +0100

    My First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ListeUserService } from './liste-user.service';
 
 
 import { AppComponent } from './app.component';
@@ -13,7 +14,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule
     ],
-  providers: [],
+  providers: [ListeUserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
